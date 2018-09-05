8d5c8da68bff929cd0b40dfefc5625b8f9882cb0
Christoph Stockinger <hello@christophstockinger.de>
2018-09-05 21:38:00 +0200

    fix: update namespace

--- src/modules/footer/index.js
+++ src/modules/footer/index.js
@@ -1,16 +1,16 @@
 import './footer'
 
-import BaseComponent, { namespace } from '@util/base/component.js'
+import BaseComponent from '@util/base/component.js'
 
 export const NAME = 'Footer'
-const CLASS = `${namespace}-mFooter`
+const CLASS = `cs-mFooter`
 
 export const ClassName = {
   ROOT: CLASS
 }
 
 export const Selector = {
-  ROOT: `${ClassName.ROOT}`
+  ROOT: `.${ClassName.ROOT}`
 }
 
 export default class Footer extends BaseComponent {
