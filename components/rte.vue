8b9c18f54ecd034e7fe615ad66aa2cfa1946b9be
Christoph Stockinger <cs@christophstockinger.de>
2020-02-29 16:50:18 +0100

    feat(components/rte): add rte component

---
+++ components/rte.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="content" class="cs-c-Rte" v-html="content"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.cs-c-Rte {
+  width: 100%;
+  height: auto;
+
+  p {
+    @include cs-typography-paragraph;
+
+    margin-bottom: $cs-spacing-m;
+
+    strong,
+    b {
+      font-weight: $cs-font-weight-bold;
+    }
+
+    em,
+    i {
+      font-style: italic;
+    }
+
+    a {
+      font-weight: $cs-font-weight-bold;
+      color: $cs-color-green;
+      text-decoration: none;
+    }
+  }
+}
+</style>
