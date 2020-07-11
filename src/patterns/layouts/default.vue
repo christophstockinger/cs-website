a2bc6993f8ffab4f8ff9c37286d5f5850c77f2a5
Christoph Stockinger <cs@christophstockinger.de>
2020-07-11 18:18:45 +0200

    fix(layouts): design adjustments

--- src/patterns/layouts/default.vue
+++ src/patterns/layouts/default.vue
@@ -7,17 +7,29 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
+<style lang="scss">
 .l-Page {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
   width: 100%;
+  height: auto;
+
+  @include cs-respond-to(m) {
+    min-height: 100vh;
+  }
 }
 
 .l-Main {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
   width: 100%;
+  height: auto;
 }
 </style>
