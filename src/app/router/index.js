f4f5225cb786344870ddd972e5ebbf81eb94141c
Christoph Stockinger <cs@christophstockinger.de>
2020-07-11 18:19:15 +0200

    fix(router): remove imprint route

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,22 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const IndexPage = () => import('@cs/pages/index.vue')
-const ImprintPage = () => import('@cs/pages/imprint.vue')
-const ErrorPage = () => import('@cs/pages/error.vue')
+const IndexPage = () =>
+  import(/* webpackChunkName: "cs-index" */ '@cs/pages/index.vue')
+const ErrorPage = () =>
+  import(/* webpackChunkName: "cs-error" */ '@cs/pages/error.vue')
 
 const routes = [
   {
     path: '/',
     name: 'Index',
     component: IndexPage,
-    meta: {
-      layout: 'default'
-    }
-  },
-  {
-    path: '/imprint',
-    name: 'Imprint',
-    component: ImprintPage,
     meta: {
       layout: 'default'
     }
